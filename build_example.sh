522d667e90cf8e6084e6defe62731611ea20f3bd
chaoticgd <chaoticgd@protonmail.com>
2018-12-23 23:27:33 +0000

    Use -Wall compiler option for examples and tests

--- build_example.sh
+++ build_example.sh
@@ -1,4 +1,4 @@
 set -e
 cd examples/$1
-g++ $1.cpp -o $1 -std=c++17 -I../../include
+g++ $1.cpp -o $1 -std=c++17 -I../../include -Wall
 ./$1
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 set -e
 cd tests
-g++ $(find . -name "*.cpp") -o runtest -std=c++17 -I../include -lgtest -lgtest_main -lpthread
+g++ $(find . -name "*.cpp") -o runtest -std=c++17 -I../include -lgtest -lgtest_main -lpthread -Wall
 ./runtest
 rm runtest
